0da661c6e3c22ba9610628c7e397ebbaf4be7580
AleksandrKhalezov <remark.mark@mail.ru>
2017-02-19 17:21:22 +0300

    dev4 complete

---
+++ src/DEV4diff/Merger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Frequency
+{
+    /// <summary>
+    /// Class to gather strings
+    /// </summary>
+    class Merger
+    {
+        private string curretnText;
+        private ArrayList partsOfText = new ArrayList();
+        
+        private void MergeText ()
+        {
+            partsOfText.Add(0, currentText);
+            if (partsOfText > 1)
+            {
+                for (int i = 1; i < partsOfText.Size(); i++)
+                {
+                    currentText = new StringBuilder(partsOfText.get(i) + " ").Append(currentText).ToString();
+                }
+            }
+        }
+
+        public string Merge(string newText)
+        {
+            currentText = newText;
+            MergeText();
+            Console.WriteLine(currentText);
+            return currentText;
+        }
+    }
+}
---
+++ src/DEV4diff/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Frequency
+{
+    class Program
+    {
+        /// <summary>
+        /// Entry point in programm
+        /// uses infinity cycle
+        /// after 3 iterations could be interrupted
+        /// </summary>
+        static void Main(string[] args)
+        {
+            int operationCounter = 0;
+            string receivedText;
+            Inputer inputer = new Inputer();
+            Merger merger = new Frequency.Merger();
+            Outputer outputer = new Outputer();
+
+            while(operationCounter <= 3)
+            {
+                operationCounter++;
+                receivedText = inputer.InputString();
+                receivedText = merger.Merge(receivedText);
+                Parser parser = new Parser(receivedText);
+
+                if ( receivedText.Equals("exit", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    break;
+                }
+                else
+                {
+                    outputer.printInConsole(new FrequencyCounter(parser.Parsetext()));
+                    Console.WriteLine("Words with over 5 letters: ");
+                    outputer.printInConsole(new WordsCounter(parser.Parsetext()));
+                }
+                Console.WriteLine("After 3 types you can type \"exit\" to stop the program.");
+            }
+        }
+}
