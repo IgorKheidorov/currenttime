488252d320dcb2ca3c343f808de75937f0b14e41
AleksandrKhalezov <remark.mark@mail.ru>
2017-03-03 13:18:29 +0300

    dev9 is complete

---
+++ src/dev9/Inputer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DEV_9
+{
+    /// <summary>
+    /// Class fills the field to search
+    /// </summary>
+    class Inputer
+    {
+        public string InputAndTrimPath()
+        {
+            string path = string.Empty;
+            Console.WriteLine("Enter the path:");
+            path = Console.ReadLine();
+            return path.Trim(); // Delete all spaces to safety
+        }
+        public string InputAndTrimSection()
+        {
+            string section = string.Empty;
+            Console.WriteLine("Enter the section:");
+            section = Console.ReadLine();
+            return section.Trim();
+        }
+        public string InputAndTrimKey()
+        {
+            string key = string.Empty;
+            Console.WriteLine("Enter the key:");
+            key = Console.ReadLine();
+            return key.Trim();
+        }
+    }
+}
